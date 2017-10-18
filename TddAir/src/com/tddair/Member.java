22679f2c794a0f93f5fc5e9ac0a282544a4bd20b
Saikumar Kalyankrishnan <saikumar_kalyankrish@dell.com>
2017-10-18 15:41:53 -0500

    Adding Tests for Status and Miles

--- TddAir/src/com/tddair/Member.java
+++ TddAir/src/com/tddair/Member.java
@@ -4,15 +4,46 @@ public class Member {
 	
 	private String username;
 	private String email;
+	private String status;
+	private int ytd;
+	private int balanceMiles;
 	
 	public Member (String username, String email) {
 		this.username = username;
 		this.email = email;
+		this.status = "Red";
+		this.ytd = 0;
+		this.balanceMiles = 10000;
 	}
 
 	public Member getMember(String username) {		
 	    
 		return new Member(username, "email");
+	}
+
+	public String getEmail() {
+		// TODO Auto-generated method stub
+		return "a@a.com";
+	}
+
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return "ABC";
+	}
+
+	public String getStatus() {
+		// TODO Auto-generated method stub
+		return "Red";
+	}
+
+	public int getYTDMiles() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getBalanceMiles() {
+		// TODO Auto-generated method stub
+		return 10000;
 	}
 		
 	
--- TddAir/test/com/tddair/WhenRegisteringMember.java
+++ TddAir/test/com/tddair/WhenRegisteringMember.java
@@ -25,11 +25,38 @@ public class WhenRegisteringMember {
 	public void shouldBeRegistered() {
 		//Verify
 		assertNotNull(member);
-		
 	}
+	
+	@Test
+	public void shouldHaveCorrectEmail() {
+		//Verify
+		assertEquals("a@a.com", member.getEmail());
+	}
+	
+	@Test
+	public void shouldHaveCorrectUsername() {
+		//Verify
+		assertEquals("ABC", member.getUsername());
+	}
+	
+	@Test
+	public void shouldHaveRedStatus() {
+		//Verify
+		assertEquals("Red", member.getStatus());
+	}
+	
+	@Test
+	public void shouldHave0YTDMiles() {
+		//Verify
+		assertEquals(0, member.getYTDMiles());
+	}
+	
+	@Test
+	public void shouldHave10KBalanceMiles() {
+		//Verify
+		assertEquals(10000, member.getBalanceMiles());
+	}
+	
 }
 
 
-//shouldHaveCorrectEmail
-//shouldHaveCorrectUsername
-//
